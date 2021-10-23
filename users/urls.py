12596ad568ce52cb2507e1e102c9c990ba0acf55
henrique77 <henriquecaires@outlook.com>
2021-10-23 10:51:52 -0300

    Criando pagina de login

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from users.views import dashboard
 
 urlpatterns = [
+    url(r"^accounts/", include("django.contrib.auth.urls")),
     url(r"^dashboard/", dashboard, name="dashboard"),
 ]
